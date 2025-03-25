9ba51225d6cfa973e8eceab395204a518a26b1c1
Heitorhj <heitorjuliobatista@gmail.com>
2025-03-25 10:57:26 -0300

    código que valida a entrada de cadeia de caracteres

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string permission = "Admin";
+        int level = 70;
+
+        if (permission.Contains("Admin"))
+        {
+            if (level > 55)
+            {
+                Console.WriteLine("Welcome, Super Admin user.");
+        
+
+        }
+        }
+        else
+        {
+            Console.WriteLine("Welcome, Admin user");
+    
+
+    }
+
+        if (permission.Contains("Manager"))
+        {
+            if (level >= 20)
+            {
+                Console.WriteLine("Contact an Admin for acess");
+                if (level < 20)
+                {
+                    Console.WriteLine("You do not have sufficient privileges.");
+                }
+
+            else
+                    {
+                        Console.WriteLine("You do not have sufficient privileges.");
+            }
+                }
+            }
+        }
+    }
+
+
+
